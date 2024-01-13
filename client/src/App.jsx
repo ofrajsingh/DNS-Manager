0dfedb2e433b911c93f996a595945d2ec8e9a490
ofrajsingh <rajsingh00seven@gmail.com>
2024-01-13 20:00:06 +0530

    Updated Route Config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.css";
-import Landing from "./components/Landing";
+import { Outlet, useNavigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  console.log("HI");
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!isLoggedIn) navigate("/login");
+  }, []);
+
   return (
     <div className="app">
       <Navbar></Navbar>
-      <Landing></Landing>
+      <Outlet />
     </div>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./components/ErrorPage.jsx";
+import Landing from "./components/Landing.jsx";
+
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [{ path: "login", element: <Landing /> }],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="552919636650-517g4jemsukb18sc6idsn3thm1sm456i.apps.googleusercontent.com">
-      <App />
+      <RouterProvider router={router} />
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
